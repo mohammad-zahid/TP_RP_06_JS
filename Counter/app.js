345865e9d80ad8e4022b1228a3febddd4117a94c
mohammad-zahid <mdzahid742@gmail.com>
2024-05-06 14:01:07 +0530

    Counter App Done

---
+++ Counter/app.js
@@ -0,0 +1,21 @@
+let counter = 0;
+function increment(){
+  counter = counter + 1;
+  // counter++;
+  // counter += 1;
+  document.getElementById('counter').innerHTML = counter;
+  
+}
+
+function decrement(){
+  counter = counter - 1;
+  // counter--;
+  // counter -=;
+  document.getElementById('counter').innerHTML = counter;
+}
+
+
+function reset(){
+  counter = 0;
+  document.getElementById('counter').innerHTML = counter;
+}
